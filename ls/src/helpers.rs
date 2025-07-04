7f58f21a4373ec0283bd1fe53461cfaefbc37fc4
iichi <ismailichi29@gmail.com>
2025-07-04 17:25:29 +0100

    changing the ls logic (working flags just looking for a better algorithm)

---
+++ ls/src/helpers.rs
@@ -0,0 +1,179 @@
+#[allow(dead_code)]
+use chrono::DateTime;
+use chrono::Local;
+use colored::*;
+use std::io;
+use std::os::unix::fs::MetadataExt;
+use std::{ffi::OsString, fs};
+
+pub fn check_flags(args: &[&str], _file_name: OsString, current_dir: &str) -> Result<(), String> {
+    // println!("flags in the args ==> {:?}", args);
+    for (_i, arg) in args.iter().enumerate() {
+        let _ = handle_flag(current_dir, *arg);
+        break;
+        // match *arg {
+        //     "-l" => {
+        //         let _ = handle_flag(current_dir, *arg);
+        //         // break;
+        //     }
+        //     "-a" => {
+        //         //Ok(());
+        //     }
+        //     "-F" => {
+        //         println!("it is -F (F) !!!");
+        //     }
+        //     _ => {
+        //         return Err("flag not found, Try -l , -a or -F".to_string());
+        //     }
+        // }
+    }
+    Ok(())
+    // Err("flag not found, Try -l , -a or -F".to_string())
+}
+
+//ls -l
+
+pub fn handle_flag(current_dir: &str, flag: &str) -> io::Result<()> {
+    let curr_dir_data = fs::read_dir(&current_dir)?;
+    let mut is_l = false;
+    let mut is_f = false;
+    let mut is_a = false;
+    for data in curr_dir_data {
+        let data = data?;
+        let path = data.path();
+        let metadata = data.metadata()?;
+        let mode = metadata.mode();
+        let is_dir = metadata.is_dir();
+        //permissions
+        let read_write = mode_to_string(mode);
+        let mut permissions = String::new();
+        let mut final_filename = String::new();
+        let filename = path.file_name().unwrap().to_string_lossy();
+        match flag {
+            "-l" => {
+                is_l = true;
+                if metadata.is_dir() {
+                    permissions.push('d');
+                } else if metadata.is_symlink() {
+                    permissions.push('l');
+                } else {
+                    permissions.push('-');
+                }
+            }
+            "-F" => {
+                is_f = true;
+
+                final_filename.push_str(&filename);
+
+                //add based on type
+                if metadata.is_dir() {
+                    // read_write.push_front('d');
+                    final_filename.push('/');
+                    let _ = &final_filename.blue().bold();
+                } else if metadata.is_symlink() {
+                    final_filename.push('@');
+                } else {
+                    final_filename.push('-');
+                }
+            }
+            "-a" => is_a = true,
+            _ => {}
+        }
+        // if metadata.is_dir() {
+        //     // read_write.push_front('d');
+        //     permissions.push('d');
+        // } else if metadata.is_symlink() {
+        //     permissions.push('l');
+        // } else {
+        //     permissions.push('-');
+        // }
+        permissions.push_str(&read_write);
+        let size = metadata.len();
+
+        let modified = metadata.modified()?;
+        //time
+        let datetime: DateTime<Local> = DateTime::from(modified);
+        let formatted = datetime.format("%b %d %H:%M").to_string();
+        // if flag == "-F" {}
+        if !filename.starts_with(".") {
+            if is_l {
+                if is_dir {
+                    println!(
+                        "{} {} {} {}",
+                        permissions,
+                        size,
+                        formatted,
+                        colored_txt(filename.to_string())
+                    );
+                } else {
+                    println!("{} {} {} {}", permissions, size, formatted, filename);
+                }
+            } else if is_f {
+                if is_dir {
+                    print!("{} ", colored_txt(final_filename));
+                } else {
+                    print!("{} ", final_filename);
+                }
+            }
+        }
+        if is_a {
+            if is_dir {
+                print!("{} ", colored_txt(filename.to_string()));
+            } else {
+                print!("{} ", filename);
+            }
+        }
+    }
+    println!("");
+    Ok(())
+}
+
+//convert digit to valid permissions
+
+pub fn digit_to_perm(digit: u8) -> String {
+    let mut perm = String::new();
+
+    // Check read bit (4)
+    if digit & 4 != 0 {
+        perm.push('r');
+    } else {
+        perm.push('-');
+    }
+
+    // Check write bit (2)
+    if digit & 2 != 0 {
+        perm.push('w');
+    } else {
+        perm.push('-');
+    }
+
+    // Check execute bit (1)
+    if digit & 1 != 0 {
+        perm.push('x');
+    } else {
+        perm.push('-');
+    }
+
+    perm
+}
+
+//get the last 3  digits from the mode
+pub fn mode_to_string(mode: u32) -> String {
+    let octal_str = format!("{:03o}", mode & 0o777);
+    //println!("ocatal ==> {:?}", octal_str);
+    let mut result = String::new();
+
+    for ch in octal_str.chars() {
+        let digit = ch.to_digit(8).unwrap() as u8;
+        result.push_str(&digit_to_perm(digit));
+    }
+    result
+}
+
+pub fn it_contains(s: &str, flag: &str) -> bool {
+    s.contains(flag)
+}
+
+fn colored_txt(s: String) -> ColoredString {
+    s.blue().bold()
+}
--- ls/src/lib.rs
+++ ls/src/lib.rs
@@ -1,10 +1,18 @@
-use std::{ffi::OsString, fs};
-use std::io::{self, Write};
-use chrono::DateTime;
-use chrono::Local;
+use std::fs;
+pub mod helpers;
+#[allow(dead_code)]
+// pub struct file_infos {
+//     permission: String,
+//     size: String,
+//     date: String,
+//     user_name: String,
+//     group_name: String,
+//     file_name: String,
+// }
+
 pub fn ls(args: &[&str], current_dir: &str) {
     //
-    println!("ls args ==> {:?}", args);
+    // println!("ls args ==> {:?}", args);
     let curr_dir_data = fs::read_dir(&current_dir);
     if args.is_empty() {
         match curr_dir_data {
@@ -15,14 +23,8 @@ pub fn ls(args: &[&str], current_dir: &str) {
                             let file_name = entry.file_name();
                             // Skip hidden files (those starting with '.')
                             if !file_name.to_string_lossy().starts_with('.') {
-                                //io::stdout().flush().unwrap();
                                 print!("{} ", file_name.to_string_lossy().trim());
                             }
-                            //for later
-                            // else if !args[0].is_empty() && args[0] == "-l" {
-                            // print!("{} ", file_name.to_string_lossy().trim());
-                            // }
-                            // print!("{} ", file_name);
                         }
                         Err(e) => eprintln!("Error reading entry: {}", e),
                     }
@@ -32,101 +34,20 @@ pub fn ls(args: &[&str], current_dir: &str) {
             Err(e) => eprintln!("Error reading directory: {}", e),
         }
     } else {
-        // eprintln!("No args yet!");
         match curr_dir_data {
             Ok(entries) => {
                 for entry in entries {
                     match entry {
                         Ok(entry) => {
-                            // let file_name1 = ;
-                            let xx = check_flags(args, entry.file_name());
-                            match xx {
-                                Ok(val) => {
-                                    print!("{} ", val.to_string_lossy());
-                                }
-                                Err(val) => {
-                                    print!("{}", val);
-                                    break;
-                                }
-                            }
+                            let _ = helpers::check_flags(args, entry.file_name(), current_dir);
+                            break;
                         }
                         Err(e) => eprintln!("Error reading entry: {}", e),
                     }
                 }
-                println!("");
+                //println!("");
             }
             Err(e) => eprintln!("Error reading directory: {}", e),
         }
     }
-}
-
-fn check_flags(args: &[&str], file_name: OsString) -> Result<OsString, String> {
-    // println!("flags in the args ==> {:?}", args);
-    for (_i, arg) in args.iter().enumerate() {
-        match *arg {
-            "-l" => {
-                println!("it is -l (list) !!!");
-                // Ok(())
-            }
-            "-a" => {
-                // println!("it is -a (all) !!!");
-                // print!("{} ", file_name.to_string_lossy());
-                return Ok(file_name);
-            }
-            "-F" => {
-                println!("it is -F (F) !!!");
-            }
-            _ => {
-                //eprintln!("flag not found, Try : -l, -a, -F");
-                return Err("flag not found, Try -l , -a or -F".to_string());
-                // break;
-            }
-        }
-    }
-    Err("flag not found, Try -l , -a or -F".to_string())
-}
-
-fn handle_l(current_dir: &str) -> io::Result<()> {
-    let curr_dir_data = fs::read_dir(&current_dir)?;
-    for data in curr_dir_data {
-        let data = data?;
-        let path = data.path();
-        let metadata = data.metadata()?;
-        let mut permissions = String::new();
-
-        if metadata.is_dir() {
-            permissions.push('d')
-        } else {
-            permissions.push('-')
-        };
-
-        if metadata.permissions().readonly() {
-            // "r--" // simple check
-            permissions.push_str("r--");
-        } else if !metadata.permissions().readonly() {
-            permissions.push_str("rw-");
-            // "rw-" // not precise, just an indicator
-        };
-
-        let size = metadata.len();
-
-        let modified = metadata.modified()?;
-        // let datetime = modified.duration_since(UNIX_EPOCH).unwrap();
-        // let seconds = datetime.as_secs();
-        let datetime: DateTime<Local> = DateTime::from(modified);
-        let formatted = datetime.format("%b %d %H:%M").to_string();
-
-        let filename = path.file_name().unwrap().to_string_lossy();
-        let mut results : Vec<Vec<String>> = vec![];
-        let mut l_results : Vec<String> = vec![];
-        l_results.push(permissions);
-        l_results.push(size.to_string());
-        l_results.push(formatted);
-        l_results.push(filename.to_string());
-        results.push(l_results);
-        results.sort();
-        // println!("{permissions} {size:>8} {formatted} {filename}");
-        println!("{:?}", results);
-    }
-    Ok(())
 }
